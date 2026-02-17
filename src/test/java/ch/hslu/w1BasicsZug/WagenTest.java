ee8e42417e6db0452b2f36d8759ce23738ca51da
PhilipHSLU <philip.luethi@stud.hslu.ch>
2026-02-17 23:31:08 +0100

    unittests

--- src/test/java/ch/hslu/w1BasicsZug/WagenTest.java
+++ src/test/java/ch/hslu/w1BasicsZug/WagenTest.java
@@ -1,4 +1,6 @@
 package ch.hslu.w1BasicsZug;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
@@ -9,24 +11,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class WagenTest {
     @Test
     void testEqualsAndHash_Class() {
+        Wagen wA = new Wagen("A", 100);
+        Wagen wB = new Wagen("B", 200);
         EqualsVerifier.forClass(Wagen.class)
                 .withOnlyTheseFields("id")
-                //.withIgnoredFields("LOG")
+//                .withIgnoredFields("capacity", "nachfolger")
                 .suppress(Warning.NONFINAL_FIELDS)
+                .withPrefabValues(Wagen.class, wA, wB)
 //                .suppress(Warning.STRICT_INHERITANCE)
                 .verify();
     }
 
-//    @Test
-//    void testMultipleWagons_two(){
-//        Wagen zug1 = new Wagen("W1", 10,
-//                new Wagen("w2", 22));
-//    }
-
-    public static void main(String[] args) {
-        Wagen zug1 = new Wagen("W1", 10,
-                new Wagen("w2", 22));
-
-        System.out.println(zug1.getCapacity());
+    @Test
+    void testGetterAndConstructor(){
+        Wagen w1 = new Wagen("1", 30);
+        assertThat(w1.getId()).isEqualTo("1");
+        assertThat(w1.getCapacity()).isEqualTo(30);
     }
+       @Test
+    void testChain(){
+        Wagen w1 = new Wagen("1", 30);
+        Wagen w2 = new Wagen("2", 10);
+
+        w1.setNachfolger(w2);
+        assertThat(w1.getNachfolger()).isEqualTo(w2);
+    }
+
+
 }
