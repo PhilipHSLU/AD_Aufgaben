08ac73562943f74fcaea90b63cad15c40991263f
PhilipHSLU <philip.luethi@stud.hslu.ch>
2026-02-23 17:59:10 +0100

    afg 2.3 c

--- src/main/java/ch/hslu/w1_rekursion/Recursion.java
+++ src/main/java/ch/hslu/w1_rekursion/Recursion.java
@@ -11,7 +11,8 @@ public class Recursion {
     public static int counterTask2 = 0;
     public static int counterTask3 = 0;
 
-    public static void task(final int n) {
+    public static void task(final int n) throws InterruptedException {
+        long startzeit = System.currentTimeMillis();
         task1(); task1(); task1(); task1();
         for (int i = 0; i < n; i++) {
             task2(); task2(); task2();
@@ -19,27 +20,40 @@ public class Recursion {
                 task3(); task3();
             }
         }
-        LOG.info("Task1: {} | Task2: {} | Task3: {} ",
-                counterTask1, counterTask2, counterTask3);
+        long endZeit = System.currentTimeMillis();
+        long totZeit = endZeit - startzeit;
+
+        LOG.info(" bei n= {} --- Task1: {} | Task2: {} | Task3: {} | TOTAL: {} | Time: {}",
+                n, counterTask1, counterTask2, counterTask3,
+                counterTask1+counterTask2+counterTask3,
+                totZeit);
     }
 
-    public static void task1(){
+    public static void task1() throws InterruptedException {
         counterTask1 ++;
+        Thread.sleep(5);
     }
-    public static void task2(){
+    public static void task2() throws InterruptedException {
         counterTask2 ++;
-
+        Thread.sleep(5);
     }
-    public static void task3(){
+    public static void task3() throws InterruptedException {
         counterTask3 ++;
+        Thread.sleep(5);
 
     }
 
     public static void main(String[] args) {
-        task(1);
-        task(2);
-        task(3);
-        task(5);
-        task(10);
+        try {
+            task(1);
+            task(2);
+            task(3);
+            task(5);
+            // afg 2.3
+            task(100);
+            task(200);
+        }catch (InterruptedException e){
+        LOG.error("error", e);
+        }
     }
 }
